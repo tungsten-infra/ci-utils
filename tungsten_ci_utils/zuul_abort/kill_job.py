8427895173e62491c8d8d8953a4fd6d1390ed464
Your Name <you@example.com>
2018-08-23 14:13:29 +0000

    Executor-side code for zuul_abort
    Script to kill the job processes on executor

--- tungsten_ci_utils/zuul_abort/kill_job.py
+++ tungsten_ci_utils/zuul_abort/kill_job.py
@@ -1,21 +1,72 @@
+#!/usr/bin/python
 from __future__ import print_function
+import os
+import sys
 import psutil
 
 
-def kill_ancestor(pid_list):
-    """Kill the top process in the hierarchy. All the provided pids should form
-    a single process hierarchy, otherwise the function exits without doing
-    anything"""
-    top_pid = 0
-    return 0
+def find_ancestor(pid_list):
+    """Get the top process in the hierarchy"""
+    external_parent = None
+    top_pid = None
+    for pid in pid_list:
+        proc = psutil.Process(pid)
+        while proc.pid in pid_list:
+            last_pid = proc.pid
+            proc = proc.parent()
+        if proc.pid != 1:
+            if external_parent is None:
+                external_parent = proc.pid
+                top_pid = last_pid
+            else:
+                if external_parent != proc.pid:
+                    print("Multiple job process roots, exiting")
+                    sys.exit(1)
+    return top_pid
+
+
+def find_child(pid_list):
+    """Get the lowest (one of the lowest) process in the hierarchy"""
+    child_pid = None
+    parents = set()
+    for pid in pid_list:
+        proc = psutil.Process(pid)
+        parent = proc.ppid()
+        if parent in pid_list:
+            parents.add(parent)
+    for pid in pid_list:
+        if pid not in parents:
+            child_pid = pid
+    if child_pid is None:
+        try:
+            child_pid = pid_list[0]
+        except IndexError:
+            return None
+    return child_pid
 
 
 def pgrep(word):
     """Searches commandlines of running processes for word. Returns a list of
     pids"""
     pids = []
-    return []
+    for proc in psutil.process_iter():
+        cmd = ' '.join(proc.cmdline())
+        if word in cmd and proc.pid != os.getpid():
+            pids.append(proc.pid)
+            print('=======================', proc.ppid(), proc.pid, cmd[:20])
+    return pids
 
 
 if __name__ == "__main__":
-    print(pgrep("vim"))
+    job_pids = pgrep(sys.argv[1])
+    if len(job_pids) > 0:
+        # find_child will cause the JOB to fail
+        # find_ancestor will cause the job to be ABORTED and RESTARTED
+        youngest_pid = find_child(job_pids)
+    else:
+        print('No running job processes found, exiting')
+        return
+    print(youngest_pid)
+    if youngest_pid is not None:
+        youngest_proc = psutil.Process(youngest_pid)
+        youngest_proc.kill()
