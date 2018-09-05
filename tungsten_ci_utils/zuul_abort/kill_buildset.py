0602fd9c9a416eec492c33d194455e551a0118d3
root <root@ci-repo.englab.juniper.net>
2018-09-05 12:56:03 +0000

    zuul_abort: add --forever param

--- tungsten_ci_utils/zuul_abort/kill_buildset.py
+++ tungsten_ci_utils/zuul_abort/kill_buildset.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import sys
 import time
 import json
@@ -6,6 +7,7 @@ import yaml
 import requests
 from multiprocessing.dummy import Pool
 import subprocess
+import argparse
 try:
     import queue
 except ImportError:
@@ -58,8 +60,10 @@ class Retrier:
                    max_tries=3):
         returned = None
         success = False
+        if max_tries is None:
+            max_tries = -1
         retry = 0
-        while not success and retry < max_tries:
+        while not success and retry != max_tries:
             if retry > 0:
                 print("Retry, attempt {}".format(retry + 1))
             time.sleep(self.strategy.get_wait_time(retry))
@@ -115,7 +119,7 @@ def kill_job(arg):
     worker_address = worker_addresses.get(worker, worker)
     print("Attempting to kill {} on {} ({})".format(
         uuid, worker, worker_address))
-    cmd = ["ssh", "-i", ssh_key_path, "-l", "zuul", worker_address]
+    cmd = ["ssh", "-T", "-i", ssh_key_path, "-l", "zuul", worker_address, uuid]
     try:
         output = subprocess.check_output(
             cmd, stderr=subprocess.STDOUT).decode('utf8')
@@ -123,7 +127,7 @@ def kill_job(arg):
     except subprocess.CalledProcessError as cpe:
         output = cpe.output.decode('utf8')
         ret = cpe.returncode
-    print("Command {} returned code {}, output: {}".format(cmd, ret, output))
+    print("Command {} returned code {}, output:\n{}".format(cmd, ret, output))
     return ret == 0
 
 
@@ -155,13 +159,16 @@ def kill_buildset(buildset_id):
     buildset = get_buildset(buildset_id)
     if buildset is None:
         print(("kill_buildset: buildset"
-               "{} not found, exiting").format(buildset_id))
+               " {} not found, exiting").format(buildset_id))
     jobs = []
+    print('kill_buildset - killing jobs:')
     for job in buildset["jobs"]:
         if (job["end_time"] is None and job["worker"] is not None and
                 job["uuid"] is not None):
             # kill running jobs
-            jobs.append((job["uuid"], job["worker"]["name"]))
+            if job["worker"]["name"] != "Unknown":
+                print('job {name}, uuid {uuid}, worker {worker}'.format(**job))
+	        jobs.append((job["uuid"], job["worker"]["name"]))
     kill_jobs(jobs)
     return None if buildset_is_running(buildset_id) else True
 
@@ -174,30 +181,46 @@ def get_nightly_zuul_ref(branch="master"):
         for head in queue['heads'][0]:
             pretty_print(head["jobs"])
             for job in head["jobs"]:
-                print(job["uuid"])
+                print("uuid:", job["uuid"])
                 if branch in job["report_url"]:
                     return head["zuul_ref"]
     return None
 
 
 def main():
-    branch = sys.argv[1]
-    config_path = 'config.yaml'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("branch")
+    parser.add_argument("--forever", action="store_true")
+    parser.add_argument("--config", default="config.yaml")
+    args = parser.parse_args()
+
+    branch = args.branch
+
+    config_path = args.config
     try:
         with open(config_path, 'r') as config_file:
             config_loaded = yaml.load(config_file)
-    except FileNotFoundError as fnfe:
+    except IOError as ioe:
         print("Config file ({}) not found, loading empty".format(config_path))
         config_loaded = {}
     config.update(config_loaded)
     worker_addresses.update(config.get("worker_addresses", {}))
-    zuul_ref = get_nightly_zuul_ref(branch)
-    if zuul_ref is None:
-        print("Buildset for branch {} not found - exiting".format(branch))
-        sys.exit(1)
-    buildset_id = zuul_ref[1:]
-    r = Retrier(ConstantRetryStrategy(10))
-    r.retry_call(kill_buildset, [buildset_id], max_tries=15)
+    if len(branch) > 15:
+        # directly specify buildset id for testing
+        buildset_id = branch
+    else:
+        zuul_ref = get_nightly_zuul_ref(branch)
+        if zuul_ref is None:
+            print("Buildset for branch {} not found - exiting".format(branch))
+            sys.exit(1)
+        buildset_id = zuul_ref[1:]
+    r = Retrier(ConstantRetryStrategy(5))
+    if args.forever:
+        max_tries = None
+    else:
+        max_tries = 20
+    r.retry_call(kill_buildset, [buildset_id], max_tries=max_tries)
 
 
 if __name__ == '__main__':
