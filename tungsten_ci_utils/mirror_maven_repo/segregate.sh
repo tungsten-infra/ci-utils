7a451eb3e2e1f466538e70943c7a67fe50b0c616
Michal Krawczyk <michal.krawczyk@codilime.com>
2018-10-10 14:26:13 +0200

    Add CLI arguments to segregate.sh

--- tungsten_ci_utils/mirror_maven_repo/segregate.sh
+++ tungsten_ci_utils/mirror_maven_repo/segregate.sh
@@ -1,28 +1,61 @@
 #!/bin/bash
 
-VERSION='7.3.0'
+# Maven artifacts can be composed of different file groups
+# There can be .pom files, .jar files, .jar's with dependencies, .jar files containing sources or javadocs
+# A maven artifact can be any subset of these
+# This script splits a given directory of maven artifacts into four directories:
+# poms - a directory containing only .pom files
+# jars - a directory containing only .pom and .jar files and eventually a .jar with dependencies
+# sources - a directory containing only a .pom, .jar and a .jar with sources
+# javadocs - a directory containing only a .pom, .jar and a .jar with javadocs
+# all - a directory containing a .pom, .jar, a .jar with sources and a .jar with javadocs
+
+usage() {
+  echo "Usage: $0 -d <directory> -v <version>"
+  exit 1
+}
+
+while getopts "d:v:" opt; do
+  case "${opt}" in
+    d)
+      directory="${OPTARG}"
+      ;;
+    v)
+      VERSION="${OPTARG}"
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+if [ "x" == "x${directory}" ] || [ "x" == "x${VERSION}" ]; then
+  usage
+fi
+
 # NOTE: Maven artifacts have a naming scheme of '<artifactId>-<version>-<classifier>.<extension>
 # ${file%%-{VERSION}*} removes everything after and including '-${VERSION}' in the 'file' variable.
 
-for file in `ls -A1 artifacts`; do echo ${file%%-${VERSION}*} >> prefixes; done
+for file in `ls -A1 ${directory}`; do echo ${file%%-${VERSION}*} >> prefixes; done
 cat prefixes | uniq > artifact_ids
 rm prefixes
 
-mkdir 1 2 3 4 2> /dev/null
+mkdir poms jars sources javadocs all > /dev/null
 
 artifact_ids=$(cat artifact_ids)
 
 for artifact_id in ${artifact_ids}; do
-  if [ `ls -A1 artifacts | grep "^$artifact_id-${VERSION}" | wc -l` -eq 4 ]; then
-    mv "artifacts/$artifact_id-${VERSION}"* 4/
-  fi
-  if [ `ls -A1 artifacts | grep "^$artifact_id-${VERSION}" | wc -l` -eq 3 ]; then
-    mv "artifacts/$artifact_id-${VERSION}"* 3/
-  fi
-  if [ `ls -A1 artifacts | grep "^$artifact_id-${VERSION}" | wc -l` -eq 2 ]; then
-    mv "artifacts/$artifact_id-${VERSION}"* 2/
-  fi
-  if [ `ls -A1 artifacts | grep "^$artifact_id-${VERSION}" | wc -l` -eq 1 ]; then
-    mv "artifacts/$artifact_id-${VERSION}"* 1/
+  if [ `ls -A1 ${directory} | grep "^$artifact_id-${VERSION}" | wc -l` -eq 4 ]; then
+    mv "${directory}/$artifact_id-${VERSION}"* all/
+  elif [ `ls -A1 ${directory} | grep "^$artifact_id-${VERSION}-jar-with-dependencies"` ]; then
+    mv "${directory}/$artifact_id-${VERSION}"* jars/
+  elif [ `ls -A1 ${directory} | grep "^$artifact_id-${VERSION}-sources"` ]; then
+    mv "${directory}/$artifact_id-${VERSION}"* sources/
+  elif [ `ls -A1 ${directory} | grep "^$artifact_id-${VERSION}-javadoc"` ]; then
+    mv "${directory}/$artifact_id-${VERSION}"* javadocs/
+  elif [ `ls -A1 ${directory} | grep "^$artifact_id-${VERSION}" | wc -l` -eq 2 ]; then
+    mv "${directory}/$artifact_id-${VERSION}"* jars/
+  elif [ `ls -A1 ${directory} | grep "^$artifact_id-${VERSION}" | wc -l` -eq 1 ]; then
+    mv "${directory}/$artifact_id-${VERSION}"* poms/
   fi
 done
