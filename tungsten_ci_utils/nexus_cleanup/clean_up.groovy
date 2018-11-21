8480ecbc2d47f1744e18be56a30e54a31ccfa800
Bartosz Pankowski <bartosz.pankowski@codilime.com>
2018-11-21 14:45:51 +0100

    Changed structure of checks, abiliti to found out name of builds.

--- tungsten_ci_utils/nexus_cleanup/clean_up.groovy
+++ tungsten_ci_utils/nexus_cleanup/clean_up.groovy
@@ -11,11 +11,11 @@ import org.slf4j.Logger
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
+def retentionDays = 15;
 def retentionCount = 15;
-def retentionDays = 30;
 def repositoryName = 'BartsDockerRepo';
-def whitelistCore = ["latest", "ocata", "newton", "queens"].toArray();
-def whitelist = ["latest","40", "94", "122", "129", "161", "214", "309", "360"].toArray();
+def core = ["ocata", "newton", "queens", "latest"].toArray();
+def whitelisted_tag_suffixes = ["latest", "ocata", "newton", "queens", "latest","40", "94", "122", "129", "161", "214", "309", "360"].toArray();
 
 log.info(":::Cleanup script started!");
 MaintenanceService service = container.lookup("org.sonatype.nexus.repository.maintenance.MaintenanceService");
@@ -38,78 +38,58 @@ if(components != null) {
     int compCount = 0;
     def listOfComponents = ImmutableList.copyOf(components);
     def previousComp = listOfComponents.head().name();
+    def coreList = [0,0,0,0];
+    def fruitEnd = true;
     listOfComponents.reverseEach{comp ->
 
-        log.info("Processing Component - ${comp.name()}, version: ${comp.version()}");
-        log.info("----------");
-        def fruitEnd = "null";
-        for(i = 0; i < whitelistCore.length; i++){
-            def fruitJuice = whitelistCore.collect{item -> item.contains(comp.version())}
-            
-            if(fruitJuice[i] == true){
-                fruitEnd = "true";
-                log.info("Fruit Juice! "+ fruitJuice[i] + " " + comp.version());
-                log.info("----------");
-                return fruitEnd;
-            }else{
-            
-                def splited = comp.version();
-                def spl = splited.split("-");
-
-                for(j = 0; j < spl.length; j++ ){            
-                    for(k = 0; k < whitelist.length; k++){
-                        def fruit = whitelist.collect{item -> item.contains(spl[j])}
-                        if(fruit[k] == true){
-                            fruitEnd = "true";
-                            log.info(spl[j]+ " true");
-                            return fruitEnd;
-                        }else{
-                            fruitEnd = "false";
-                            log.info(spl[j]+ " false");
+        def splited = comp.version();
+        def spl = splited.split("-");
+        for (i = 0; i < core.length; i++) {
+            fruitEnd = true;
+            log.info(comp.name() + " " + core[i]);
+            if (comp.name() == core[i]) {
+                coreList[i]++;
+                if (coreList[i] > retentionCount) {
+                    for (j = spl.length - 1; j < spl.length; j++) {
+                        for (k = 0; k < whitelisted_tag_suffixes.length; k++) {
+                            def fruit = whitelisted_tag_suffixes.collect { item -> item.contains(spl[j])}
+                            if (fruit[k] == true) {
+                                fruitEnd = true;
+                                log.info(spl[j] + " true");
+                                return fruitEnd;
+                            } else {
+                                fruitEnd = false;
+                                log.info(spl[j] + " false");
+                            }
+                            
                         }
+                        
                     }
+
                 }
-            }
-        }
-        log.info("----------");
-        log.info(fruitEnd + " Result");
-        log.info("----------");
 
+                if(fruitEnd == false){
 
-        if(fruitEnd == "false"){
-            log.info("am I?");
-            log.info(previousComp);
-
-            if(previousComp.equals(comp.name())) {
-            // for test purposes check of prev name commented out
-            
-            // if(fruitEnd == "false"){
-                compCount++;
-                log.info("ComCount: ${compCount}, ReteCount: ${retentionCount}");
-
-                if (compCount > retentionCount) {
                     log.info("CompDate: ${comp.lastUpdated()} RetDate: ${retentionDate}");
                     if(comp.lastUpdated().isBefore(retentionDate)) {
                         log.info("compDate after retentionDate: ${comp.lastUpdated()} isAfter ${retentionDate}");
-                        log.info("deleting ${comp.group()}, ${comp.name()}, version: ${comp.version()}");
-
+                        log.info("deleting ${comp.name()}, version: ${comp.version()}");
                         // ------------------------------------------------
                         // uncomment to delete components and their assets
                         // service.deleteComponent(repo, comp);
                         // ------------------------------------------------
-
                         log.info("component deleted");
                         log.info("----------");
                         deletedComponentCount++;
                     }
-                }
-            } else {
-                compCount = 1;
-                previousComp = comp.name();
-            }
         }else{
-            log.info("Component skipped: ${comp.name()}");
+            log.info("Component skipped: ${comp.name()} ${comp.version()}");
         }
+            } else {
+                log.info('wuuuuut')
+            }
+
+        }        
     }
     log.info("----------");
     log.info("Deleted Component count: ${deletedComponentCount}");
