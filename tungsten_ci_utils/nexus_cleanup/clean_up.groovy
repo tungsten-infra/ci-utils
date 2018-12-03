ba94852019154eac7b4c4c537a19884d85205ea6
Bartosz Pankowski <bartosz.pankowski@codilime.com>
2018-12-03 10:16:00 +0100

    Change names of variables.

--- tungsten_ci_utils/nexus_cleanup/clean_up.groovy
+++ tungsten_ci_utils/nexus_cleanup/clean_up.groovy
@@ -32,60 +32,48 @@ if (components != null) {
     def retentionDate = DateTime.now().minusDays(retentionDays).dayOfMonth().roundFloorCopy();
     int deletedComponentCount = 0;
     def listOfComponents = ImmutableList.copyOf(components);
-    def previousComp = listOfComponents.head().name();
 
 // comp.version() expected to be in a format {{ os }}-{{ realse }}-{{ branch }}-{{ build }} e.g. rhel-queens-master-386 
 
     listOfComponents.reverseEach { comp ->
 
-        def splited = comp.version();
-        def spl = splited.split("-");
-        def build_number = spl[spl.length - 1];
+        def tag = comp.version();
+        def tagSplited = tag.split("-");
+        def build_number = tagSplited[tagSplited.length - 1];
 
         for (j = 0; j < whitelisted_tag_suffixes.length; j++) {
             if (build_number == whitelisted_tag_suffixes[j]) {
-                checkValue = true;
                 log.info("Component skipped: ${comp.name()} ${comp.version()}");
-                return checkValue;
             } else {
                 if (whitelisted_tag_suffixes.count(build_number) == 0) {
                     if (tagList.count(build_number.toInteger()) == 0) {
                         tagList.add(build_number.toInteger());
                         println tagList.sort();
-                    }                    
+                    }
                 }
             }
-        }        
+        }
     }
 
     listOfComponents.reverseEach { comp ->
-        checkValue = null;
-        def splited = comp.version();
-        def spl = splited.split("-");
-        def build_number = spl[spl.length - 1];
+        def tag = comp.version();
+        def tagSplited = tag.split("-");
+        def build_number = tagSplited[tagSplited.length - 1];
         def retentionList = tagList.subList(0, tagList.size() - 15);
 
         if (whitelisted_tag_suffixes.count(build_number) == 0) {
-            if (retentionList.count(build_number.toInteger()) > 0) {
-                checkValue = false;
+            if (retentionList.contains(build_number.toInteger())) {
+                if (comp.lastUpdated() > retentionDate) {
+                    log.info("retentionDate: ${comp.lastUpdated()} isAfter ${retentionDate}");
+                    log.info("deleting ${comp.name()}, version: ${comp.version()}");
+                    // uncomment to delete components and their assets
+                    // service.deleteComponent(repo, comp);
+                    log.info("----------");
+                    deletedComponentCount++;
+                } else {
+                    log.info("Component skipped: ${comp.name()} ${comp.version()}");
+                }
             }
-        }
-        
-        if (checkValue == false) {
-            log.info("----------");
-            log.info("CompDate: ${comp.lastUpdated()} RetDate: ${retentionDate}");
-            // if (comp.lastUpdated() > retentionDate) {
-                log.info("retentionDate: ${comp.lastUpdated()} isAfter ${retentionDate}");
-                log.info("deleting ${comp.name()}, version: ${comp.version()}");
-                // ------------------------------------------------
-                // uncomment to delete components and their assets
-                // service.deleteComponent(repo, comp);
-                // ------------------------------------------------
-                log.info("----------");
-                deletedComponentCount++;
-            // }
-        } else {
-            log.info("Component skipped: ${comp.name()} ${comp.version()}");
         }
     }
     log.info("\n\nDeleted Component count: ${deletedComponentCount} \n");
