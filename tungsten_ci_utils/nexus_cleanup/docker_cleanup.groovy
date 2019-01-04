1d86b65aa23c05b8654c35670c802ddead24fd00
bpankowski <bartosz.pankowski@codilime.com>
2019-01-04 09:42:58 +0100

    Add check after branch and build number to the logic.
    Whitelist should contain branch name and build number fe. `master-343`
    In previous version it wasn't covered.
    Add-on instruction on the top of the script.

--- tungsten_ci_utils/nexus_cleanup/docker_cleanup.groovy
+++ tungsten_ci_utils/nexus_cleanup/docker_cleanup.groovy
@@ -1,3 +1,8 @@
+// Script created for retention of private docker repo on Nexus.
+// To use that script change value of repositoryName to the name of cleaned repository.
+// Second thing to do is uncomment line 75. That will allow script to delete objects in docker registry.
+// Open your Nexus Server -> Server administration and configuration -> Tasks -> Create task -> Admin - Execute script
+// -> Choose name of a task -> change Script language to groovy -> set Task frequency -> paste script and save.
 import org.sonatype.nexus.repository.storage.StorageFacet;
 import org.sonatype.nexus.common.app.GlobalComponentLookupHelper;
 import org.sonatype.nexus.repository.maintenance.MaintenanceService;
@@ -13,7 +18,7 @@ def retentionDays = 15;
 def retentionCount = 15;
 def tagList = [];
 def repositoryName = 'BartsDockerRepo';
-def whitelisted_tag_suffixes = ["queens", "ocata", "newton", "latest", "40", "94", "122", "129", "161", "214", "309", "360"].toArray();
+def whitelisted_tag_suffixes = ["queens", "ocata", "newton", "latest", "master-40", "master-94", "master-122", "master-129", "master-161", "master-214", "master-309", "master-360"].toArray();
 log.info(":::Cleanup script started!");
 MaintenanceService service = container.lookup("org.sonatype.nexus.repository.maintenance.MaintenanceService");
 def repo = repository.repositoryManager.get(repositoryName);
@@ -39,11 +44,15 @@ if (components != null) {
     listOfComponents.reverseEach { comp ->
         def tag = comp.version();
         def tagSplited = tag.split("-");
+        log.info("tagsplit ${tag} ${tagSplited}");
         def build_number = tagSplited[tagSplited.length - 1];
-
+        def two_parts_build_number = tagSplited[tagSplited.length - 2] + "-" + tagSplited[tagSplited.length - 1];
+        log.info("new two parts ${two_parts_build_number}");
         if (whitelisted_tag_suffixes.contains(build_number)) {
             log.info("Component skipped: ${comp.name()} ${comp.version()}");
-        } else {
+        } else if (whitelisted_tag_suffixes.contains(two_parts_build_number)){
+            log.info("Component skipped: ${comp.name()} ${comp.version()}");
+        }else {
             if (tagList.count(build_number.toInteger()) == 0) {
                 tagList.add(build_number.toInteger());
                 println tagList.sort();
@@ -55,6 +64,7 @@ if (components != null) {
         def tag = comp.version();
         def tagSplited = tag.split("-");
         def build_number = tagSplited[tagSplited.length - 1];
+        def two_parts_build_number = tagSplited[tagSplited.length - 2] + "-" + tagSplited[tagSplited.length - 1];
         def retentionList = tagList;
         if(tagList.size() > retentionCount){
             retentionList = tagList.subList(0, tagList.size() - retentionCount);
@@ -63,16 +73,18 @@ if (components != null) {
             return true;
         }
         if (!whitelisted_tag_suffixes.contains(build_number)) {
-            if (retentionList.contains(build_number.toInteger())) {
-                if (comp.lastUpdated() < retentionDate) {
-                    log.info("retentionDate: ${comp.lastUpdated()} isOlder ${retentionDate}");
-                    log.info("deleting ${comp.name()}, version: ${comp.version()}");
-                    // uncomment to delete components and their assets
-                    // service.deleteComponent(repo, comp);
-                    log.info("----------");
-                    deletedComponentCount++;
-                } else {
-                    log.info("Component skipped: ${comp.name()} ${comp.version()}");
+            if (!whitelisted_tag_suffixes.contains(two_parts_build_number)) {
+                if (retentionList.contains(build_number.toInteger())) {
+                    if (comp.lastUpdated() < retentionDate) {
+                        log.info("retentionDate: ${comp.lastUpdated()} isOlder ${retentionDate}");
+                        log.info("deleting ${comp.name()}, version: ${comp.version()}");
+                        // uncomment to delete components and their assets
+                        // service.deleteComponent(repo, comp);
+                        log.info("----------");
+                        deletedComponentCount++;
+                    } else {
+                        log.info("Component skipped: ${comp.name()} ${comp.version()}");
+                    }
                 }
             }
         }
