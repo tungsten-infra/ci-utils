d7efb7fdf52c6f77c03ae49af954dd040225f5a0
bpankowski <bartosz.pankowski@codilime.com>
2019-02-18 15:49:52 +0100

    Fixed typo and code block.

--- tungsten_ci_utils/nexus_empty_repos_cleanup/empty_repos.groovy
+++ tungsten_ci_utils/nexus_empty_repos_cleanup/empty_repos.groovy
@@ -2,6 +2,7 @@ import org.sonatype.nexus.repository.storage.Asset
 import org.sonatype.nexus.repository.storage.Query
 import org.sonatype.nexus.repository.storage.StorageFacet
 import org.sonatype.nexus.repository.raw.internal.RawFormat
+import org.joda.time.DateTime;
 
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
@@ -18,6 +19,7 @@ assert repo: "Repository ${request.repoName} does not exist";
 def retentionDays = 14;
 def retentionDate = DateTime.now().minusDays(retentionDays).dayOfMonth().roundFloorCopy();
 StorageFacet storageFacet = repo.facet(StorageFacet);
+def counter = 0;
 def tx = storageFacet.txSupplier().get();
 try {
     tx.begin()
@@ -29,21 +31,21 @@ try {
 
     assets.each { asset ->
         def name = asset.name();
-        def nameSplited = name.split("/");       
-       
+        def nameSplited = name.split("/");
         if(nameSplited.length.toString() == '3'){
             if(asset.lastUpdated() < retentionDate){
                 log.info("Deleting asset ${asset.name()}")
-            // tx.deleteAsset(asset);
+                counter++;
+                tx.deleteAsset(asset);
                 if (asset.componentId() != null) {
                     log.info("Deleting component for asset ${asset.name()}")
                     def component = tx.findComponent(asset.componentId());
-                // tx.deleteComponent(component);
-                }   
+                    tx.deleteComponent(component);
+                }
             }
         }
-    }   
-
+    }
+    log.info(counter.toString());
     tx.commit()
 
 } catch (Exception e) {
