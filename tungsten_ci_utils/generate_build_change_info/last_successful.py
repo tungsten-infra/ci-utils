b6a60b7048b02f19b9fe9d99d5a2e72e085fcc1f
mstencel <mateusz.stencel@codilime.com>
2019-03-14 20:42:55 +0100

    Rewrite last_successful.py

--- tungsten_ci_utils/generate_build_change_info/last_successful.py
+++ tungsten_ci_utils/generate_build_change_info/last_successful.py
@@ -3,13 +3,14 @@ import argparse
 import MySQLdb
 import logging
 import json
+import re
 
 log = logging.getLogger(__name__)
 
 def set_logging():
     log.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        '%(asctime)s - %(levelname)s - %(message)s')
     console = logging.StreamHandler()
     console.setLevel(logging.DEBUG)
     console.setFormatter(formatter)
@@ -18,26 +19,45 @@ def set_logging():
 set_logging()
 
 def get_json_data(file):
-    json_file = open(file).read()
-    data = json.loads(json_file)
+    with open(file, 'r') as json_file:
+        data = json.load(json_file)
     return data
 
 def close_db_exit_err(db):
+    #raise
     if db is not None:
         db.close()
+        log.debug('db connection closed')
     sys.exit(1)
+
+def get_current_buildset_info(cur,query,branch,build_number):
+    cur.execute(query, (branch, build_number))
+    buildset_id, end_time = list(cur)[0]
+    return buildset_id,end_time
+
+def get_last_successful_buildset_info(cur,query,branch,current_id,end_time):
+    cur.execute(query, (branch, current_id, end_time))
+    success_id = list(cur)[0][0]
+    return success_id
+
+def get_log_url(cur,query,id):
+    cur.execute(query,(id,))
+    log_url = list(cur)[0][0]
+    return log_url
+
+def build_number_from_log_url(regex,log_url):
+    return re.search(regex, log_url).group(1)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--credentials-json", action="append")
+    parser.add_argument("--credentials-json")
     parser.add_argument("branch")
-    parser.add_argument("build_number")
+    parser.add_argument("build_number", type=int)
     args = parser.parse_args()
-    branch = str(args.branch)
-    credentials_json = args.credentials_json[0]
 
-    build_number = og_build_number = int(args.build_number)
-    build_number -= 1
+    branch = str(args.branch)
+    build_number = args.build_number
+    credentials_json = args.credentials_json
 
     db_config = get_json_data(credentials_json)
 
@@ -50,46 +70,57 @@ def main():
             port=db_config["port"]
         )
         cur = db.cursor()
-        log.debug('connected to db')
     except:
         log.error('database connection error')
         sys.exit(1)
 
-    while build_number > 0:
-        query = """
-        SELECT result FROM zuul_buildset WHERE id IN 
-        (SELECT buildset_id FROM zuul_build WHERE log_url LIKE CONCAT('%%periodic-nightly%%/', %s, '/%s%%'))
-        """
+    query_get_buildset = """
+    SELECT max(buildset_id), max(end_time) FROM zuul_build
+    WHERE log_url LIKE CONCAT('%%', %s, '/%s/%%')
+    """
 
-        log.debug('checking if buildset %s was successful', build_number)
+    query_success_id = """
+    SELECT max(buildset_id) FROM zuul_buildset
+    JOIN zuul_build ON zuul_buildset.id = zuul_build.buildset_id
+    WHERE zuul_buildset.result = 'SUCCESS'
+    AND zuul_buildset.pipeline = 'periodic-nightly'
+    AND zuul_buildset.ref LIKE CONCAT('refs/heads/', %s)
+    AND zuul_buildset.id < %s
+    AND zuul_build.end_time < %s;
+    """
 
-        try:
-            cur.execute(query, (branch, build_number))
-            result = list(cur)
-        except:
-            log.error('Query execution error')
-            close_db_exit_err(db)
+    query_success_log_url = """
+    SELECT log_url FROM zuul_build
+    WHERE buildset_id = %s
+    ORDER BY end_time DESC
+    LIMIT 1
+    """
 
-        if len(result) > 0:
-            if result[0][0] == 'SUCCESS':
-                log.debug('last successful buildset before %s found, number: %s', og_build_number, build_number)
-                print(build_number)
-                break
-            elif result[0][0] == 'FAILURE':
-                log.debug('buildset %s was a failure', build_number)
-            else:
-                log.warning('unknown buildset result for current iteration (build number %s)', build_number)
-        else:
-            log.error('buildset number %s not found in the database, aborting', build_number)
-            close_db_exit_err(db) # we exit here, assuming there are no gaps in (incremental) build numbers
+    try:
+        log.debug('getting zuul buildset_id and end_time for current build number')
+        current_buildset_id, end_time = get_current_buildset_info(cur, query_get_buildset, branch, build_number)
 
-        build_number -= 1
+        log.debug('getting id of last successful buildset before current')
+        last_success_id = get_last_successful_buildset_info(cur, query_success_id, branch, current_buildset_id, end_time)
 
-    else:
-        log.error('last successful buildset not found')
+        log.debug('getting log url of last successful buildset')
+        log_url = get_log_url(cur, query_success_log_url, last_success_id)
+    except MySQLdb.OperationalError:
+        log.error('error executing query, aborting')
+        close_db_exit_err(db)
+    except IndexError:
+        log.error('invalid values fetched from database, aborting')
+        close_db_exit_err(db)
+    except:
+        log.error('unknown error (not raising exception)')
         close_db_exit_err(db)
 
     db.close()
+    
+    regex = '(?<=' + branch + '\/)(.*?)(?=\/)'
+    last_successful = build_number_from_log_url(regex,log_url)
+
+    print(last_successful)
 
 if __name__ == '__main__':
     main()
