fb7a1a05bb8cad1229ce3edc85c359deba914202
mstencel <mateusz.stencel@codilime.com>
2019-03-15 17:07:48 +0100

    Readability (tips from @sszwed)

--- tungsten_ci_utils/generate_build_change_info/last_successful.py
+++ tungsten_ci_utils/generate_build_change_info/last_successful.py
@@ -8,11 +8,11 @@ import re
 log = logging.getLogger(__name__)
 
 def set_logging():
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
     formatter = logging.Formatter(
         '%(asctime)s - %(levelname)s - %(message)s')
     console = logging.StreamHandler()
-    console.setLevel(logging.DEBUG)
+    console.setLevel(logging.INFO)
     console.setFormatter(formatter)
     log.addHandler(console)
 
@@ -50,44 +50,45 @@ def main():
             port=db_config["port"]
         )
         cur = db.cursor()
+        log.info('connected to db')
     except:
         log.error('database connection error')
         sys.exit(1)
 
     query_get_buildset = """
-    SELECT max(buildset_id), max(end_time) FROM zuul_build
-    WHERE log_url LIKE CONCAT('%%', %s, '/%s/%%')
-    """
+                              SELECT max(buildset_id), max(end_time) FROM zuul_build
+                              WHERE log_url LIKE CONCAT('%%', %s, '/%s/%%')
+                         """
 
     query_success_id = """
-    SELECT max(buildset_id) FROM zuul_buildset
-    JOIN zuul_build ON zuul_buildset.id = zuul_build.buildset_id
-    WHERE zuul_buildset.result = 'SUCCESS'
-    AND zuul_buildset.pipeline = 'periodic-nightly'
-    AND zuul_buildset.ref LIKE CONCAT('refs/heads/', %s)
-    AND zuul_buildset.id < %s
-    AND zuul_build.end_time < %s
-    """
+                            SELECT max(buildset_id) FROM zuul_buildset
+                            JOIN zuul_build ON zuul_buildset.id = zuul_build.buildset_id
+                            WHERE zuul_buildset.result = 'SUCCESS'
+                                AND zuul_buildset.pipeline = 'periodic-nightly'
+                                AND zuul_buildset.ref LIKE CONCAT('refs/heads/', %s)
+                                AND zuul_buildset.id < %s
+                                AND zuul_build.end_time < %s
+                       """
 
     query_success_log_url = """
-    SELECT log_url FROM zuul_build
-    WHERE buildset_id = %s
-    ORDER BY end_time DESC
-    LIMIT 1
-    """
+                                SELECT log_url FROM zuul_build
+                                WHERE buildset_id = %s
+                                ORDER BY end_time DESC
+                                LIMIT 1
+                            """
 
     try:
         log.debug('getting zuul buildset_id and end_time for current build number')
         cur.execute(query_get_buildset, (branch, build_number))
-        current_buildset_id, end_time = list(cur)[0]
-                
+        current_buildset_id, end_time = cur.fetchone()
+
         log.debug('getting id of last successful buildset before current')
         cur.execute(query_success_id, (branch, current_buildset_id, end_time))
-        last_success_id = list(cur)[0][0]
+        last_success_id = cur.fetchone()
 
         log.debug('getting log url of last successful buildset')
         cur.execute(query_success_log_url,(last_success_id,))
-        log_url = list(cur)[0][0]
+        log_url = str(cur.fetchone())
 
     except MySQLdb.OperationalError:
         log.error('error executing query, aborting')
@@ -104,6 +105,7 @@ def main():
     finally:
         cur.close()
         db.close()
+        log.info('db connection closed')
     
     last_successful = get_build_number_from_log_url(log_url,branch)
     print(last_successful)
